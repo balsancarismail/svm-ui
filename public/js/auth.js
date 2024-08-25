715fe4589325972bf4345714c1315ce79b2974cb
ismailsancarbal <ismail.sancar.bal@mobileitm.com>
2024-08-25 19:04:00 +0300

    auth js bug fixed

--- public/js/auth.js
+++ public/js/auth.js
@@ -37,30 +37,32 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    async function sendLoginRequest(url, email, password, lat, lng) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password, lat, lng })
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            localStorage.setItem('accessToken', data.access_token);
-            localStorage.setItem('refreshToken', data.refresh_token);
-
-            localStorage.setItem('user', JSON.stringify({
-                id: data.user.id,
-                name: data.user.name,
-                email: data.user.email,
-                friends: data.user.friends, // Kullanıcının arkadaşları
-                locations: data.user.locations // Kullanıcının konumları
-            }));
-
-            window.location.href = 'index.html';
-        } else {
-            alert('Login failed!');
-        }
-    }
-
 });
+
+
+
+async function sendLoginRequest(url, email, password, lat, lng) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password, lat, lng })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('accessToken', data.access_token);
+        localStorage.setItem('refreshToken', data.refresh_token);
+
+        localStorage.setItem('user', JSON.stringify({
+            id: data.user.id,
+            name: data.user.name,
+            email: data.user.email,
+            friends: data.user.friends, // Kullanıcının arkadaşları
+            locations: data.user.locations // Kullanıcının konumları
+        }));
+
+        window.location.href = 'index.html';
+    } else {
+        alert('Login failed!');
+    }
+}
